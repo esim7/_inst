368048030b67c1349d269865b5369f4c579e18b8
esim7 <makishev@yahoo.com>
2020-07-03 15:46:36 +0600

    Добавьте файлы проекта.

---
+++ _inst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _inst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
